4f722a076e923a4f79b8057371c10f3dd22f903b
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 14:43:28 -0400

    Update solveIntermediateValueTheorem.py

--- solveIntermediateValueTheorem.py
+++ solveIntermediateValueTheorem.py
@@ -1,4 +1,4 @@
-# take user input and solve the intermediate value theorem using python
+
 def solveIntermediateValueTheorem(x):
     if x < 0:
         return 2 * x ** 2 + 3 * x + 1
@@ -7,10 +7,7 @@ def solveIntermediateValueTheorem(x):
     else:
         return x ** 2
 def main():
-    # get user input
     x = float(input("Enter a value for x: "))
-    # solve the intermediate value theorem
     y = solveIntermediateValueTheorem(x)
-    # print the result
     print("y =", y)
 main()
