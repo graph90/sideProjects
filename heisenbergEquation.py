31727febb45e1e08336957cc08294f5e60930f36
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 14:44:27 -0400

    Update heisenbergEquation.py

--- heisenbergEquation.py
+++ heisenbergEquation.py
@@ -2,18 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 def heisenbergEquation():
-
-    A = float(raw_input('Enter the operator: '))
-    # Define the Hamiltonian
-    H = float(raw_input('Enter the Hamiltonian: '))
-    # Define the time
-    t = float(raw_input('Enter the time: '))
-    # Define the imaginary unit
-    i = float(raw_input('Enter the imaginary unit: '))
-    # Define the Planck constant
-    hbar = float(raw_input('Enter the Planck constant: '))
-    # Calculate the Heisenberg equation
+    A = float(input('Enter the operator: '))
+    H = float(input('Enter the Hamiltonian: '))
+    t = float(input('Enter the time: '))
+    i = float(input('Enter the imaginary unit: '))
+    hbar = float(input('Enter the Planck constant: '))
     heisenbergEquation = np.gradient(A) + (1/hbar)*np.cross(H, A)
-    # Print the Heisenberg equation
-    print ('Heisenberg equation: ', heisenbergEquation)
+    print('Heisenberg equation: ', heisenbergEquation)
 heisenbergEquation()
