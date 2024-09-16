32a6592d60a39e19238b89c84d737ba0c327ff14
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 15:03:58 -0400

    Update asciiToInt.py

--- asciiToInt.py
+++ asciiToInt.py
@@ -1,16 +1,11 @@
 def main():
-    print "This program converts a sequence of ASCII codes into a string of text."
-    print
-
-    # get the message to encode
-    inString = raw_input("Please enter the ASCII-encoded message: ")
-
-    # loop through each substring and build Unicode message
+    print ("This program converts a sequence of ASCII codes into a string of text.")
+    inString = input("Please enter the ASCII-encoded message: ")
     message = ""
     for numStr in inString.split():
-        codeNum = int(numStr) # convert digits to a number
-        message = message + chr(codeNum) # concatentate character to message
+        codeNum = int(numStr)
+        message = message + chr(codeNum)
 
-    print "The decoded message is:", message
+    print ("The decoded message is:", message)
 
 main()
