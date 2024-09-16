4c7b7b650252d988a840dc66acee1ef4a1a8996d
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 14:50:34 -0400

    Update meanValueTheorem.py

--- meanValueTheorem.py
+++ meanValueTheorem.py
@@ -1,10 +1,7 @@
 def meanValueTheorem():
-    # get user input
     a = float(input("Enter a value for a: "))
     b = float(input("Enter a value for b: "))
     c = float(input("Enter a value for c: "))
-    # solve the mean value theorem
     y = (f(b) - f(a)) / (b - a)
-    # print the result
     print("y =", y)
 meanValueTheorem()
