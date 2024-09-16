6fce340eb43258e3df8baf286f4e6d79db5a0091
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 14:43:03 -0400

    Update shannonInformationTheory.py

--- shannonInformationTheory.py
+++ shannonInformationTheory.py
@@ -1,11 +1,7 @@
 def shannonInformationTheory():
-    # get the entropy from the user
     entropy = float(input("Enter the entropy: "))
-    # get the bandwidth from the user
     bandwidth = float(input("Enter the bandwidth: "))
-    # calculate the Shannon Information Theory
     shannonInformationTheory = entropy*bandwidth
-    # print the Shannon Information Theory
     print("The Shannon Information Theory is: ", shannonInformationTheory)
 def main():
     shannonInformationTheory()
