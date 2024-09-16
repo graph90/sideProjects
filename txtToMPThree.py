3d0df64ada0da96a90c9862642b06d06d7ea8c0d
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 14:42:18 -0400

    Update txtToMPThree.py

--- txtToMPThree.py
+++ txtToMPThree.py
@@ -1,16 +1,11 @@
 import pyttsx3
 
-# Initialize the TTS engine
 engine = pyttsx3.init()
 
-# Set the voice properties
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)  # change the index to use a different voice
-
-# Read the text file
+engine.setProperty('voice', voices[1].id)
 with open('input.txt', 'r') as file:
     text = file.read().replace('\n', '')
 
-# Convert text to speech and save as an audio file
 engine.save_to_file(text, 'output.mp3')
 engine.runAndWait()
