92e9abb8f37caf5cfad72a5c8b25904b687bc227
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 15:01:23 -0400

    Update manhattanDistancee.py

--- manhattanDistancee.py
+++ manhattanDistancee.py
@@ -1,6 +1,5 @@
 def manhattan_distance(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
     
-# Example usage
 distance = manhattan_distance(1, 2, 4, 6)
-print(distance)  # Output: 7
+print(distance)
