aa7e282c40e25b8d8a99d8088c74c6fc5d799448
Nathaniel Armogan <54459997+graph90@users.noreply.github.com>
2024-09-16 15:02:35 -0400

    Update bayesTheorem.py

--- bayesTheorem.py
+++ bayesTheorem.py
@@ -1,19 +1,8 @@
-# bayes theorem
 
 def main():
-    # get the probability of the first event
     p1 = float(input("Enter the probability of the first event: "))
-
-    # get the probability of the second event
     p2 = float(input("Enter the probability of the second event: "))
-
-    # get the probability of the first event given the second event
     p1g2 = float(input("Enter the probability of the first event given the second event: "))
-
-    # calculate the probability of the second event given the first event
     p2g1 = (p1g2 * p2) / p1
-
-    # display the probability of the second event given the first event
     print("The probability of the second event given the first event is", p2g1)
-
 main()
